f3016fdf87dd27a588cf8346d5a4173774b43bf9
Nykyta Koblianskyi <ok.service.company@gmail.com>
2024-07-08 10:20:40 +0200

    features

--- src/components/features/item.tsx
+++ src/components/features/item.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 interface FeaturesProps {
     backgroundImg: string;
@@ -8,21 +8,23 @@ interface FeaturesProps {
 }
 
 export const FeaturesItem: React.FC<FeaturesProps> = ({ backgroundImg, title, description }) => {
-        
+
     return (
-        <div className='featuresitem'>
-                <div className="featuresitem-wrapp">
-                    <Image src={backgroundImg}
+        <div className="featuresitem">
+            <div className='featuresitem-wrapp'>
+                <Image
+                    src={backgroundImg}
                     alt={title}
                     width={480}
                     height={675}
-                    objectFit="contain"
-                    className='featuresitem-img' />
-                        <h1 className="featuresitem-title">{title}</h1>
+                    objectFit="cover"
+                    className='featuresitem-img'
+                />
+                <h1 className="featuresitem-title">{title}</h1>
+                <div className='featuresitem-description'>
+                    <p className="featuresitem-text">{description}</p>
                 </div>
-                <div className="featuresitem-description">
-                    {/* <p className="featuresitem-text">{description}</p> */}
-                </div>
+            </div>
         </div>
-    )
-    }
+    );
+};
